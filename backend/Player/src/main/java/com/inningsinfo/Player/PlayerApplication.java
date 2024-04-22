2a40d2d890ee2a22d5fc2aecd8cd5eea3d14a884
Harish kumar <harishkumarp3303@gmail.com>
2024-04-22 07:55:57 +0530

    server changes

--- backend/Player/src/main/java/com/inningsinfo/Player/PlayerApplication.java
+++ backend/Player/src/main/java/com/inningsinfo/Player/PlayerApplication.java
@@ -2,10 +2,12 @@ package com.inningsinfo.Player;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlayerApplication {
+public class PlayerApplication extends SpringBootServletInitializer{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerApplication.class, args);
 	}
